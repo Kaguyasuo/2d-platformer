f038b3298f03bf863f2eb87d9329ab3559f8c4a0
Kaguyasuo <williamkhorton@gmail.com>
2022-12-30 20:53:25 -0500

    Moving Platform Finished

--- 2D Platformer Tutorial/Assets/Scripts/WaypointFollower.cs
+++ 2D Platformer Tutorial/Assets/Scripts/WaypointFollower.cs
@@ -2,19 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlatformMovement : MonoBehaviour
+public class WaypointFollower : MonoBehaviour
 
 {
-    private Animator platformAnimator;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject[] waypoints;
+    private int currentWaypoint = 0;
+
+    [SerializeField] private float platformVelocity = 2f;
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Vector2.Distance(waypoints[currentWaypoint].transform.position, transform.position) < .1f)
+        {
+            currentWaypoint++;
+            if (currentWaypoint >= waypoints.Length)
+            {
+                currentWaypoint = 0;
+            }
+        }
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypoint].transform.position, Time.deltaTime * platformVelocity);
+
     }
 }
